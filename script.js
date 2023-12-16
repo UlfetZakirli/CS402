80b2b55fd9f6a53ba585daf3bb296b3a4104dab0
UlfetZakirli <ulfetzakirli@gmail.com>
2023-12-16 14:10:45 +0400

    feat: Introduction

---
+++ script.js
@@ -0,0 +1,268 @@
+// console.log('Ulfat Zakirli');
+
+// let, var, const - keywords (to declare variables)
+// let username1 = 'Reshad'
+// var username2 = 'Nihad'
+// const username3 = 'Ferid'
+
+// console.log(username1);
+// console.log(username2);
+// console.log(username3);
+
+// let: keyword,
+// username: variable name,
+// 'Reshad': value
+
+// const companyName = 'Code Academy'
+
+// let todayDate = '16.12.2023'
+// todayDate = '17.12.2023'
+
+// console.log('1', username);
+// console.log('2', username);
+
+// var user = 'Nihad'
+// var user = 'Ferid'
+// var user = 'Ferid'
+// var user = 'Ferid'
+// var user = 'Ferid'
+// var user = 'Ferid'
+// var user = 'Ferid'
+// var user = 'Ferid'
+// var user = 'Ferid'
+// var user = 'Ferid'
+// var user = 'Ferid'
+// var user = 'Ferid'
+// var user = 'Ferid'
+// var user = 'Ferid'
+// var user = 'Nicat'
+
+// let user = 'Ulfat'
+// console.log(user);
+// firstName,lastName, age
+
+// Data types in js (there are 8 data types in js)
+// primitiv - 7
+// non-primitiv - 1
+
+// primitiv data type
+// string(all text and number in quotes),
+// number(all numbers),
+// bigInt(big data number),
+// symbol(symbol),
+// boolean(true,false),
+// undefined(undefined),
+// null(null)
+
+// let age = 23083402384384n
+
+// let isAdmin = Symbol(3)
+// console.log(isAdmin);
+
+// username(string), userAge(number), isAdmin(boolean), salary(null), date(undefined)
+
+// let user = 'Ulfat'
+// console.log(user);
+// {
+//     let user = 'Kenan'
+//     console.log(user);
+// }
+// {
+//     let user = 'Murad'
+//     console.log(user);
+// }
+// {
+//     let user = 'Kenan'
+//     console.log(user);
+// }
+
+// let firstname = 'Kenan'
+// let firstName = 'Murad'
+// let FirstName = 'Anar'
+
+// name conversions in js
+// 1.camelCase (firstName)
+// 2.Pascal   (FirstName)
+// 3.snakeCase (first_name)
+// 4.kebabCase (first-name)
+
+//operators (+,-,/,*,%,**)
+
+// let a = 20
+// let b = 10
+// let c = a / b
+// console.log(c);
+
+// let user1 = 'Reshad'
+// let user2 = 'Fuad'
+// console.log(user1 + "" + user2);
+
+// let f = undefined;
+// let b = '8'
+// let c = 11
+// let d = 7
+// let a = 5
+// console.log(a + f + b + c + d);
+
+// console.log(typeof f);
+// console.log(typeof f);
+// console.log(typeof f);
+// console.log(typeof f);
+
+
+
+// let a = 9
+// let b = 2
+// console.log(a ** b);
+
+
+
+// =, ==, ===
+
+// let age = 23
+// let age2 = age
+// let age3 = age
+// let age4 = age
+// let age5 = age
+
+
+// console.log(7 == '7'); (check only value)
+// console.log(7 === '7'); (value and type)
+
+
+
+// let age = '23'
+
+// if (age >= 28) {
+//     console.log('1');
+// } else if (age == 23) {
+//     console.log('2');
+// } else if (age == 23) {
+//     console.log('3');
+// } else {
+//     console.log('4 ');
+// }
+
+
+// let username = 'Ulfat'
+
+// if (username == 'ulfat') {
+//     console.log('1');
+// } else {
+//     console.log('2');
+// }
+
+
+
+// (all positiv and negative numbers is true, only 0 is false)
+// (all string is true)
+
+
+// falsy values - 0, null, undefined,NaN,false,'',""
+
+
+// let string = 'isAdmin'
+
+// let = 'Reshed'
+
+// if ('isAdmin' == string) {
+//     console.log('something');
+// } else {
+//     console.log('else');
+// }
+
+
+// let a = 'a'
+// let b = 'b'
+
+// let a, b
+// a = 'a'
+// b = 'c'
+
+
+// age,username,isAdmin
+
+// if (age > 19) {
+//     console.log('You are allowed!');
+// } else if (username == 'Ulfat') {
+//     console.log('Name is correct!');
+// } else if (isAdmin) {
+//     console.log('You are admin!');
+// } else {
+//     console.log('sorry!');
+// }
+
+// let age = 23
+// switch (age) {
+//     case 20:
+//         console.log('1');
+//         break;
+//     case 24:
+//         console.log('2');
+//         break;
+//     case 23:
+//         console.log('3');
+//     case 23:
+//         console.log('4');
+//     default:
+//         console.log('default');
+//         break;
+// }
+
+
+
+
+
+
+// Bitwise operators (&&, ||, !)
+
+// & -and
+// | - or
+// !- not
+
+
+let age = 20
+// if (age > 27 || age > 18 || age > 100 || age == 'Ulfat' || age == nul) {
+//     console.log('age');
+// }
+
+
+// if (age != '20') {
+//     console.log(age);
+// }
+
+// ==, ===
+// !=, !==
+
+
+// let isAdmin = 'null'
+
+// true version             //false version
+// if (isAdmin === true)   // if (isAdmin === false)
+// if (isAdmin == false)   // if (isAdmin == true)
+// if (!isAdmin)           // if (isAdmin)
+
+
+
+
+// if (isAdmin = '8') {
+//     isAdmin = '9'
+//     console.log(isAdmin);
+// }
+
+// console.log(isAdmin);
+
+
+
+// if (isAdmin) {
+//     console.log(isAdmin);
+// }
+
+// || - show first true value  (if not then show last value)
+//  && - show first false value (if not then show last value)
+
+
+// console.log(9 && 90 && 'undefined' && false);
+// console.log('false' || 'undefined' || 0);
+
+
