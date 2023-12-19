a983f943cafb23f43e9ba795d4fa1025b86d54b9
UlfetZakirli <ulfetzakirli@gmail.com>
2023-12-19 10:19:56 +0400

    feat: task 1

---
+++ homework/01.Task_1/script.js
@@ -0,0 +1,146 @@
+////////////// TASK 1 //////////////
+
+// 1.Multiply 12 with 8, and console the result:
+
+// 2.Divide 10 by 2 add result to variables x, and console x:
+
+// 3.Create two variables called num1 and num2, assign the value 20 to num1 and value 17 to num2 then addition(+) them in console.
+
+/* 4.Create variables called name, surname and year assign values to them ('your name', 'your surname', ' your birth of year'),
+  then add them to varibale name (info) and console info. */
+
+// 5.Console the remainder(%) when 17 is divided by 12.
+
+// 6.Create a varibale called "city name" (camelCase). Assign value Baku, then call variable again
+//  and reassign value Gence,console result.
+
+
+
+////// TASK 2 //////////////
+
+// let a = 1, b = 1;
+// let c = ++a;
+// let d = b++;
+
+// console.log(c); // ?
+// console.log(d); // ?
+// console.log(b); // ?
+
+
+////////////// TASK 3 //////////////
+
+// let a = 2;
+// let x = 1 + (a *= 2);
+// console.log(x); // ?
+
+
+////////////// TASK 4 //////////////
+
+// console.log('test1', test); // ?
+// {
+//     let test = "something"
+//     console.log('test2', test); // ?
+// }
+// console.log('test3', test);  // ?
+
+
+// console.log('test1', test);  // ?
+// {
+//     var test = "something"
+//     console.log('test2', test); // ?
+// }
+// console.log('test3', test); // ?
+
+
+////////////// TASK 5 //////////////
+
+//Find the type of all the following cases
+
+// let name = "Ulfat"
+// let surname = Zakirli
+// let year = 2000
+// year = "2000"
+// let city;
+// let qualification = null
+// let obj = { name: 'ulfat' }
+// let arr = ['a', 'b', 'c']
+
+
+////////////// TASK 6 //////////////
+const arr1 = [9, 1, 5, 8, 9, 9, 3, 22, 11, 12, 32, 8]
+// 1.Sum all the numbers in 'arr1'
+// 2.Add two numbers (10,88) to end of arr1 (push methods)
+// 3.Remove first two numbers from arr1
+// 4.Add three numbers (0,9,11) in front of arr1(unshift)
+// 5.Remove four numbers in front of arr1(shift)
+
+
+////////////// TASK 7 //////////////
+const arr2 = ["Murad", "Anar", "Rufet", 'Anar', 'Gulshen', 'Nermin', "Ilkin", "Fuad", "Anar"]
+// 1.Console values from "Rufet" to "Fuad"
+// 2.Change Gulshen to Rovshen 
+// 3.Console each name with map
+// 4.Console only names which is Anar
+// 5.Console all names where name is Anar change it to "Perviz"
+// 6.Console second to last value of arr2
+// 7.Console length of arr2
+
+
+
+////////////// TASK 8 //////////////
+const arr3 = ["9", 10, 100, 11, 'true', 'undefined', true, null, 'something', false]
+// (use typeof), like this: filter((item) => typeof item === 'string')
+
+// 1.Show only numbers
+// 2.Show only true values
+// 3.Show only false values
+// 4.Show only strings
+
+
+////////////// TASK 8 //////////////
+const numbers = [1, 2, 4, 5, 3, 2, 5, 7, 8, 9, 11, 12, 22, 24, 25]
+// 1.Console (only even numbers)
+// 2.Console (only odd numbers)
+
+
+////////////// TASK 9 //////////////
+const arr4 = [10, "29", 10, 100, 11, 'true', false, 'undefined', true, null, 'something', false]
+
+// 1.Console all values of arr4 -
+// if value is number add it 10, if value is string add to it value + is string,if is value falsy value add to it value+ is falsly value(with return word)
+
+// Example: [10,'true',false]
+// Result: [20, "true is string", "false is falsy value"]
+
+// 2.Sum all numbers of arr4
+// 3.Count only the values that are true
+// 4.Count only the values that are string
+// 5.Count only the values that are false
+
+
+////////////// TASK 10 //////////////
+// 1.Create a object add your name, surname, age and city.
+// 2.Then change name to "Jhon"
+
+const obj = {
+    email: 'ulfat@gmail.com',
+    info: {
+        gender: 'Male',
+        loc: {
+            city: "Baku",
+            street: 'Nizami 22',
+            education: {
+                "uni name": "ADNSU",
+            }
+        }
+    }
+}
+
+// 3.Console gender
+// 4.Console city
+// 5.Console uni name
+
+
+
+
+
