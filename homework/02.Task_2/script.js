89826e6c8a1db52ec88361c2db981c271a4c9e98
UlfetZakirli <ulfetzakirli@gmail.com>
2023-12-26 23:34:06 +0400

    feat: task_2

---
+++ homework/02.Task_2/script.js
@@ -0,0 +1,93 @@
+// let fullName = 'Ulfat Zakirli Rovshen'
+
+// 1) Ad soyad ata adinizi butov sheklde goturun ve array sheklinde herflerine ayirin(split)
+// 2) FullName'də soyadinizi adinizin onune keciren alqoritm yazin
+// 3) Alinan yeni arrayi stringe cevirin(join)
+
+
+// let arr = [4, 5, 2, 1, 5, 2, 5, 3, 5, 6, 2, 1, 6, 2, 6, 2, 5, 3, 2, 7, 4, 6, 4, 5, 6, 2, 5, 6, 3, 7, 3, 7]
+
+// QEYD - Bu tasklarda "arr" arrayindan istifade edilecekdir.....
+
+//4) Yuxaridaki arrayda 5 reqeminin nece defe tekrarlandigini tapin
+//5) Yuxaridaki arraydaki butun reqemlerin cemini tapin
+//6) arrayda tekrar olunan reqemleri artan sira ile duzun
+//7) arraydaki en boyuk reqemi tapin ve nece defe tekrarlandigini gosterin(Math.max)
+//8) Adinizdaki herflerin sayini tapin ve hemin sayin arrayda olub olmamasini yoxlayin Meselen Ulfat adinda 5 herf var ve 5 arrayda var(includes)
+//9) arraydaki ilk 3 e bolunende qaliqda 2 alinan reqemi ve hemin reqemin arraydaki indexini tapin
+//10) arraydaki en boyuk reqemin ilk indexini tapin
+//11) 4 reqeminin arrayin hansi indexlerinde oldugunu gosterin
+//12) 5 reqeminin arraydaki en boyuk ve en kicik indexlerini tapin
+//13) "arr" - arrayindan reqemleri 2 den boyuk olan yeni array yaradin ve alinmish arrayla "arr" arrayinin uzunluqlari ferqini hesablayin
+//14) 7 reqeminin indexleri cemini tapin.
+
+
+let arr2 = [
+    {
+        name: 'test',
+        key: 1
+    },
+    {
+        name: 'task',
+        key: 2
+    },
+    {
+        name: 'tanqo',
+        key: 3
+    },
+    {
+        name: 'like',
+        key: 4
+    },
+    {
+        name: 'task',
+        key: 5
+    },
+    {
+        name: 'trust',
+        key: 6
+    },
+    {
+        name: 'test',
+        key: 7
+    },
+    {
+        name: 'last',
+        key: 8
+    },
+    {
+        name: 'tanqo',
+        key: 9
+    },
+    {
+        name: 'elephant',
+        key: 10
+    },
+    {
+        name: 'love',
+        key: 11
+    },
+    {
+        name: 'small',
+        key: 12
+    },
+    {
+        name: 'little',
+        key: 13
+    },
+]
+
+
+//Aşağıdakı hər bir taskı function icərisində yazın.
+
+//QEYD Bu tasklarda arr2 istifade edilecekdir
+// 16 arr2 de "name"-i "t" herfi ile bashlayan obyektleri yeni arraya yigin
+// 17 arr2 de "name"-i "t" herfi ile bashlayib "t" herfi ile biten obyektlerin sayini tapin
+// 18 arr2 de "name"-i "t" herfi ile bashlayib "t" herfi ile biten obyektlerin "key"- lerinin cemini tapin
+// 19 arr2 de "name"-i "e" herfi ile biten obyeklerdeki name-in deyerini "SuperDev" sozu ile evezleyin.
+// 20 arr2 de "name"-i en uzun olan obyektin key-i ni tapin
+// 21 arr2 de "name"-i en uzun olan obyektin indexin kvadratini hesablayin
+// 22 arr2 de "name"-inin uzunlugu 4 olan obyektlerden ibaret yeni array yaradin.
+// 23 arr2 de en boyuk "key" - i olan obyektin "name"-i ni tapin
+// 24 arr2 de terkibinde 2 'L' herfi olan obyekt(ler)in index(ler)ini tapin.
+// 25 arr2 de terkibinde 2 't' herfi olan obyekt(ler)in key(ler)ini tapin.
